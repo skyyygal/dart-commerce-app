89cc62e7b6b7910a371a2fd09ad1d2240922d0c5
skyyygal <heeramnakp@gmail.com>
2025-12-06 13:41:01 +0530

    E-commerce app ini

---
+++ bin/sample_ecommerce_app.dart
@@ -0,0 +1,5 @@
+import 'package:sample_ecommerce_app/sample_ecommerce_app.dart' as sample_ecommerce_app;
+
+void main(List<String> arguments) {
+  print('Hello world: ${sample_ecommerce_app.calculate()}!');
+}
---
+++ test/sample_ecommerce_app_test.dart
@@ -0,0 +1,8 @@
+import 'package:sample_ecommerce_app/sample_ecommerce_app.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
